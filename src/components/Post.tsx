a0be3154859e8e240b112c5c5792be8f3e91f888
Arup Debnath <1805043@ugrad.cse.buet.ac.bd>
2023-08-03 01:50:44 +0600

    Designed the layout for post

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -3,11 +3,59 @@
 import React from "react";
 
 const Post = () => {
-  console.log("hello")
+  const post = {
+    user: {
+      name: "John Doe",
+      image: "https://picsum.photos/200/300"
+    },
+    title: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    timestamp: "2021-09-01T00:00:00.000Z",
+    hierarchy: "BUET/CSE",
+    body: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    tags: ["tag1", "tag2", "tag3"],
+    upvotes: 10,
+    replies: 5
+  }
   return (
     <div
-      className="mt-8 flex h-screen flex-row items-start justify-between bg-red-500">
-      Post
+      className="m-5">
+      <div className="flex items-center justify-between">
+        <div>
+          <img src={post.user.image} alt="user image" className="w-12 h-12 rounded-full" />
+        </div>
+        <div>
+          <p>1 hour ago</p>
+        </div>
+        <div>
+          <p>{post.hierarchy}</p>
+        </div>
+        <div>
+          <p>Announcement</p>
+        </div>
+      </div>
+      <div>
+        <h5 className="">{post.title}</h5>
+      </div>
+      <div>
+        <p>{post.body}</p>
+      </div>
+      <div className="flex justify-between">
+        <div>
+          <p>upvotes: {post.upvotes}</p>
+        </div>
+        <div>
+          <p>replies: {post.replies}</p>
+        </div>
+        <div>
+          <p>share</p> 
+        </div>
+        <div>
+          <p>save</p>
+        </div>
+        <div>
+          <p>report</p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/home-container/PostsContainer.tsx
+++ src/components/home-container/PostsContainer.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Post from "../Post";
 
 const PostsContainer = () => {
   return (
     <div
       className="w-10/12 lg:w-7/12 flex rounded-xl flex-col bg-green-300">
-      PostsContainer
+      <Post />
     </div>
   );
 }
