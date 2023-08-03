b4fec53311d824ea87ee579da8d242aae0e62e11
Noob101 <sajiborton6437@gmail.com>
2023-08-03 19:50:23 +0600

    course tree

--- src/components/home-container/CoursesTree.tsx
+++ src/components/home-container/CoursesTree.tsx
@@ -1,14 +1,1 @@
-'use client'
 
-import React from "react";
-
-const CoursesTree = () => {
-  return (
-    <div
-      className="flex h-[32rem] w-1/6 flex-row items-start justify-between bg-white rounded-xl">
-      <h2 className="text-xl tracking-wide font-bold text-gray-500 my-5 mx-auto">My Courses</h2>
-    </div>
-  );
-}
-
-export default CoursesTree;
