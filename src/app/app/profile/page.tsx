39fa411282a866d766dc9f01b7c69d0f7e489fb2
Arup Debnath <1805043@ugrad.cse.buet.ac.bd>
2023-08-21 12:35:15 +0600

    Profile page design

--- src/app/app/profile/page.tsx
+++ src/app/app/profile/page.tsx
@@ -2,7 +2,20 @@
 const ProfilePage = () => {
   return (
     <>
-      profile page
+      <form className="mt-10">
+        <label className="m">
+          Set Bio:
+          <input className="ml-10 w-96 h-10 rounded-md" type="text" name="bio" />
+        </label>
+        <br /> <br />
+        <label>
+          Address:
+          <input className="ml-10 w-96 h-10 rounded-md" type="text" name="address" />
+        </label>
+        <br />
+        <br />
+        <button className="w-32 h-12 bg-accent2 text-white rounded-md">Update profile</button>
+      </form>
     </>
   )
 };
